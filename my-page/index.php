7c600252d74d6c58419554ddc9383e88b63c1263
Mario Brusarosco <mariobrusarosco@hotmail.com>
2015-12-06 15:45:14 -0200

    SETTING THE MAIN STRUCTURE

---
+++ my-page/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Mario Brusarosco</title>
+
+    <!--  bootstrap -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap/bootstrap.min.css">
+
+    <!-- my CSS -->
+    <link rel="stylesheet" type="text/css" href="css/my-page.css">
+
+
+  </head>
+  <body>
+
+    <main>
+
+      <nav>
+
+      </nav>
+
+      <section>
+
+
+      </section>
+
+    </main>
+    <!--  jQuery -->
+
+    <!--  my scripts -->
+
+  </body>
+</html>
