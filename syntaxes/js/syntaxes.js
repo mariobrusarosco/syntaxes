6636b6d2122f9e600f64ee6074f9f95bbc77be19
Mario Brusarosco <mariobrusarosco@hotmail.com>
2015-12-29 11:59:08 -0200

    EVENT HANDLER TO PERFORM A QUERY: START

--- syntaxes/js/syntaxes.js
+++ syntaxes/js/syntaxes.js
@@ -1,7 +1,22 @@
 //GLOBAL OBJECTS//
+var Search = {
+  query    : function(event){
+              event.preventDefault();
+              console.log("working");
 
+              //START AN AJAX CALL//
+              $.ajax(
+                "../helpers/search-syntax.php",
+                
+              );
+              // return false;
+            }
+};
 //GLOBAL OBJECTS//
 
 $(document).ready(function(){
-  console.log("working"); 
+
+//SEARCH SYNTAX BUTTON//
+$("main").on("click","#submit_search_btn",Search.query);
+
 });
