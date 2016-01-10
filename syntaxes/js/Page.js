b8d1c235b4cdba909c720964b91aa77933b04d36
Mario Brusarosco <mariobrusarosco@hotmail.com>
2016-01-10 14:53:08 -0200

    TWO METHODS WORKING => addError, addSuccess

--- syntaxes/js/Page.js
+++ syntaxes/js/Page.js
@@ -27,43 +27,122 @@ var Log    = {
 // OBJECT ORIENTED VERSION //
 class LogOOP{
   constructor(){
-    this.allSucess   = [];
-    this.allErrors   = [];
+    this.allSuccess   = {
+                          'name'       : 'Successfull',
+                          'visible'    : true,
+                          'dataLogged' : []
+    };
+    this.allErrors   = {
+                          'name'       : 'Failed',
+                          'visible'    : true,
+                          'dataLogged' : []
+    };
   }
 
   addError(errorType,errorDesc,url){
-    this.allErrors.push({
+    //INSERT DATA INTO THE ARRAY THAT IS INSIDE THE OBJECT//
+    this.allErrors.dataLogged.push({
                       'error type'         : errorType    || "no error type was passed",
                       'error description'  : errorDesc    || "no error description was passed",
                       'address'            : url          || "no address was passed"
                     });
-    this.showLog();
+    //AFTER THAT, CALL .showLog() TELLING IT THE TYPE OF ACTION TO BE SHOWN ON CONSOLE. CONSOLE SHOULD SHOW ALL DATA INSIDE THE ARRAY 'dataLogged'//
+    // this.showLog(this.allErrors['name']);
+    //RETURN THE OBJECT ITSELF//
     return this;
   }
-
+  //
   addSuccess(response,url){
-    this.allSucess.push({
-                      'response'  : response,
+    this.allSuccess.dataLogged.push({
+                      'response'  : response                  || "no response came back",
                       'status'    : 'ajax call successfully',
-                      'address'   : url
+                      'address'   : url                       || "no address was passed"
                    });
-    this.showLog();
+    // this.showLog();
     return this;
   }
 
-  showLog(){
-    if(!this.allErrors.length && !this.allSucess.length) console.log("No data logged yet");
-    
-    for(var index in this.allErrors){
-      console.log("Error # " + index);
-      console.log(" \t" + Object.keys(this.allErrors[index])[0] + " ==> " + this.allErrors[index]['error type']);
-      console.log(" \t" + Object.keys(this.allErrors[index])[1] + " ==> " + this.allErrors[index]['error description']);
-      console.log(" \t" + Object.keys(this.allErrors[index])[2] + " ==> " + this.allErrors[index]['address']);
-      console.log("\n");
-    }
+  // PARAMETERS
+  // actions : string (caseinsensitive)
+    // case string : must be => 'all' || 'sucessfull' || 'failed'
+   showLog(actions){
+    // IF NOTHING WAS PASSAED AS PARAMETER//
+      if(!actions){
+        console.log("No parameter was passed to showLog()"); //TELL THE ERROR//
+        return this;                                        //CLOSE THE PROGRAM//
+      }
+    // IF SOMETHING WAS PASSAED...//
+      else{
+          /* STRUCTURE EXAMPLE
+          typeOfAction{
+                      'nameProp'       : 'nameVal',
+                      'visibleProp'    : 'visibleVal',
+                      'dataLoggedProp' : [
+                                            === OBJECT #0 ====
+                                          {
+                                            'key1'  : "value1",
+                                            'key2'  : "value2"
+                                          },
+                                            === OBJECT #1 ====
+                                          {
+                                            'key1'  : "value1",
+                                            'key2'  : "value3",
+                                            'key3'  : "value3"
+                                          }
+                                      ]
+                  }
+          STRUCTURE EXAMPLE */
 
-    return this;
-  }
+        //STORE ALL TYPES OF ACTIONS INSIDE A VARIABLE//
+        var action = [this.allErrors,this.allSuccess];
+        //FOR EACH TYPE OF ACTION//
+        for(var index in action){
+          //CHECK IF THE TYPE OF ACTION IS SET TO BE VISIBLE//
+          if(action[index]['visible'] === true){
+            //IF IT IS...SHOW THE TYPE OF ACTIONS//
+            console.log(action[index]['name'] + " Actions");
+            //GO THE THE ACTION ARRAY NAMED 'dataLogged'.//
+            var dataLogged = action[index]['dataLogged'];
+              //IF THIS ARRAY IS EMPTY... TELL IT ON CONSOLE AND JUMP TO THE NEXT ITERATION//
+              // if(dataLogged.length === 0){
+              //   console.log("Nothing stored inside of " + action[index]['name'] + " Actions yet");
+              //   continue;
+              // };
+            //FOR EACH LOG INSIDE OF '.dataLogged//
+            for(var index2 in dataLogged){
+              //STORE AN ARRAY CONTAINING THE LOG'S KEYS INSIDE A VARIABLE//
+              var logKeys = Object.keys(dataLogged[index2]);
+              //FOR EACH KEY INSIDE A LOG//
+              for(var index3 in logKeys){
+                console.log("\t\t" + logKeys[index3] + " ==> " + dataLogged[index2][logKeys[index3]]);
+              }
+              console.log("\n");
+            }
+          }
+        }
+      }
+    //IF NOTHING IS LOGGED YET//
+  //   if(!this.allErrors.dataLogged.length && !this.allSucess.dataLogged.length){
+  //     console.log("No data logged yet"); //TELL THE USER//
+  //     return this; //CLOSE THE PROGRAM//
+  //   }
+  //
+  //   for()
+  //   //IF NO SUCCESS LOG IS STORE, JUST SHOW THE ERRORS//
+  //   if(!this.allSuccess.length){
+  //     for(var index in this.allErrors){
+  //       console.log("Error # " + index);
+  //       console.log(" \t" + Object.keys(this.allErrors[index])[0] + " ==> " + this.allErrors[index]['error type']);
+  //       console.log(" \t" + Object.keys(this.allErrors[index])[1] + " ==> " + this.allErrors[index]['error description']);
+  //       console.log(" \t" + Object.keys(this.allErrors[index])[2] + " ==> " + this.allErrors[index]['address']);
+  //       console.log("\n");
+  //     }else{
+  //       for()
+  //     }
+  //   }
+  //
+  //   return this;
+  }//END OF .showLog()//
 
 }
 
